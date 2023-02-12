2db4127770fca03ea661d6c5d18ba2d4849dc0ae
lymims <lymims@outlook.com>
2023-02-12 16:18:14 +0800

    Task - PlanCycle 添加 Hourly

--- LilsWorkApi/LilsWorkApi/Models/PlanCycle.cs
+++ LilsWorkApi/LilsWorkApi/Models/PlanCycle.cs
@@ -1,7 +1,11 @@
 ﻿namespace LilsWorkApi.Models
 {
+    /// <summary>
+    /// 更改此处枚举后，应该及时更新前端相应的枚举定义
+    /// </summary>
     public enum PlanCycle
     {
+        Hourly,
         Daily,
         Weekly,
         Monthly,
